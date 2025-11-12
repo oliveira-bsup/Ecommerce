0ea59065a2f855c38a3f8db3a7cc67167d8673d1
oliveira-bsup <leonardo.o.ribeiro6@aluno.senai.br>
2025-11-12 08:59:48 -0300

    Upload

--- main.js
+++ main.js
@@ -9,14 +9,19 @@ fetch("https://dummyjson.com/products")
             const conteiner = (document.getElementById("container"));
             conteiner.innerHTML += `
             <div class="card">
-            <div class="rating">Avaliação: ${produtos[i].rating}</div>
-            <img src="${produtos[i].thumbnail}" alt="${produtos[i].title}">
             <h3>${produtos[i].title}</h3>
+            <img src="${produtos[i].thumbnail}" alt="${produtos[i].title}">
+            <section class="miniCard">
+            <div class="rating">Avaliação: ${produtos[i].rating}</div>
             <p>${produtos[i].description}</p>
-            <div class="prince">Preço:R$ ${produtos[i].price}</div>
+            <div class="prince">Preço: $${produtos[i].price}</div>
+            </section>
+            <div>
             <button class="btn-comprar">Comprar</button>
             <button class="btn-carrinho" id="adicionar">Carrinho</button>
-            </div>`
+            </div>
+            </div>
+            `
         }
         
     })
